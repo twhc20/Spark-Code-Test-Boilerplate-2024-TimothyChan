0a3831b6149621c1a7ab53271bc23d4a1bc30c3f
twhc20 <twhc20@bath.ac.uk>
2024-05-29 16:35:49 +0100

    Feat: Implemented todolist functionality to submit todos and then list them

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ToDo struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
+}
+
+var toDoList = []ToDo{} 
 
 func main() {
 	// Your code here
+	http.HandleFunc("/", ToDoListHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	// Your code here
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS") // CORS 
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type") // CORS
+
+	switch r.Method {
+		case "OPTIONS": // CORS preflight request 
+			w.WriteHeader(http.StatusOK) // response 200
+		case "GET": // Sends back the list of ToDos
+			w.Header().Set("Content-Type", "application/json")
+			err := json.NewEncoder(w).Encode(toDoList)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest) // response 400
+				return
+			}
+			w.WriteHeader(http.StatusOK) // response 200
+		case "POST": // Adds a new ToDo to the list
+			var toDo ToDo
+
+			err := json.NewDecoder(r.Body).Decode(&toDo)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest) // response 400
+				return
+			}
+			toDoList = append(toDoList, toDo)
+			w.WriteHeader(http.StatusOK) // response 200 
+		default:
+			http.Error(w, "Method not allowed", http.StatusInternalServerError) // response 500
+	}
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,11 @@ import Todo, { TodoType } from './Todo';
 
 function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [refresh, setRefresh] = useState<boolean>(false); // Used to refresh the todo list
+  const [form, setForm] = useState({title: '', description: ''}) // Used to store the form data
 
-  // Initially fetch todo
+  // Initially fetch todo 
+  // Added refresh state to refresh the todo list after adding a new todo
   useEffect(() => {
     const fetchTodos = async () => {
       try {
@@ -22,7 +25,53 @@ function App() {
     }
 
     fetchTodos()
-  }, []);
+  }, [refresh]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent page reload
+
+    // Trim the form data
+    var title = form.title.trim();
+    var description = form.description.trim();
+    // If title OR description is empty, return and do not add the todo
+    // Could argue that description can be empty, but for this example, we will require it as specifed in the yaml file
+    if (title === '' || description === '') {
+      console.log('Error adding todo: invalid input');
+      return;
+    }
+
+    // put the form data into a newTodo object
+    var newTodo = { title: form.title, description: form.description };
+
+    // Send POST request to server
+    try {
+      const response = await fetch('http://localhost:8080/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newTodo),
+      });
+
+      if (response.status === 200) { // If todo added successfully
+        console.log ('Todo added successfully');
+        setRefresh(!refresh); // Refresh the todo list
+        setForm({title: '', description: ''}); // Clear the form
+        return;
+      } else if (response.status === 400){ // If invalid input
+        console.log('Error adding todo: invalid input');
+        return;
+      }
+      else { // all other cases
+        console.log('Error adding todo');
+        return;
+      }
+
+    } catch (e) {
+      console.log('Could not connect to server. Ensure it is running. ' + e);
+    }
+
+  }
 
   return (
     <div className="app">
@@ -41,9 +90,20 @@ function App() {
       </div>
 
       <h2>Add a Todo</h2>
-      <form>
-        <input placeholder="Title" name="title" autoFocus={true} />
-        <input placeholder="Description" name="description" />
+      <form onSubmit={handleSubmit}>
+        <input 
+        placeholder="Title" 
+        name="title" 
+        autoFocus={true}
+        value={form.title}
+        onChange={e => setForm({...form, title: e.target.value})}
+        />
+        <input 
+        placeholder="Description" 
+        name="description" 
+        value={form.description}
+        onChange={e => setForm({...form, description: e.target.value})}
+        />
         <button>Add Todo</button>
       </form>
     </div>
